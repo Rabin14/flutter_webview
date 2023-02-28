e7b06d67f656884c45e4d64ae3380ca884bc5b57
Rabin14 <56728714+Rabin14@users.noreply.github.com>
2023-02-28 08:11:32 +0530

    ok

---
+++ lib/exam.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class exam extends StatefulWidget {
+  const exam({Key? key}) : super(key: key);
+
+  @override
+  State<exam> createState() => _examState();
+}
+
+class _examState extends State<exam> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/webView.dart
+++ lib/webView.dart
@@ -1,62 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class WebViewScreen extends StatefulWidget {
+class WebViewScreen extends StatelessWidget {
   final String url;
 
   WebViewScreen({required this.url});
-
-  @override
-  _WebViewScreenState createState() => _WebViewScreenState();
-}
-
-class _WebViewScreenState extends State<WebViewScreen> {
-  late WebViewController _webViewController;
-  bool _isLoadingPage = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Web View"),
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () async {
-            if (await _webViewController.canGoBack()) {
-              _webViewController.goBack();
-            } else {
-              Navigator.pop(context);
-            }
-          },
-        ),
       ),
-      body: SafeArea(
-        child: Stack(
-          children: [
-            WebView(
-              initialUrl: widget.url,
-              javascriptMode: JavascriptMode.unrestricted,
-              onWebViewCreated: (WebViewController webViewController) {
-                _webViewController = webViewController;
-              },
-              onPageStarted: (String url) {
-                setState(() {
-                  _isLoadingPage = true;
-                });
-              },
-              onPageFinished: (String url) {
-                setState(() {
-                  _isLoadingPage = false;
-                });
-              },
-            ),
-            if (_isLoadingPage)
-              Center(
-                child: CircularProgressIndicator(),
-              ),
-          ],
+      body: WillPopScope(
+        onWillPop: () async {
+          final bool canGoBack = await _webViewController.canGoBack();
+          if (canGoBack) {
+            _webViewController.goBack();
+            return false;
+          } else {
+            return true;
+          }
+        },
+        child: WebView(
+          initialUrl: url,
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController webViewController) {
+            _webViewController = webViewController;
+          },
         ),
       ),
     );
   }
+
+  late WebViewController _webViewController;
 }
